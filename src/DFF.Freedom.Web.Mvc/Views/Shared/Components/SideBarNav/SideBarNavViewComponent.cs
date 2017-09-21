5d3d9ffd41e657d09dcc9354edc297fb726bb408
dafeifei0218 <ytr19860218@163.com>
2017-09-21 17:31:21 +0800

    侧栏导航注释

--- src/DFF.Freedom.Web.Mvc/Views/Shared/Components/SideBarNav/SideBarNavViewComponent.cs
+++ src/DFF.Freedom.Web.Mvc/Views/Shared/Components/SideBarNav/SideBarNavViewComponent.cs
@@ -5,11 +5,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DFF.Freedom.Web.Views.Shared.Components.SideBarNav
 {
+    /// <summary>
+    /// 侧栏导航 视图组件
+    /// </summary>
     public class SideBarNavViewComponent : FreedomViewComponent
     {
         private readonly IUserNavigationManager _userNavigationManager;
         private readonly IAbpSession _abpSession;
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="userNavigationManager">用户导航管理类</param>
+        /// <param name="abpSession"></param>
         public SideBarNavViewComponent(
             IUserNavigationManager userNavigationManager,
             IAbpSession abpSession)
@@ -18,6 +26,11 @@ namespace DFF.Freedom.Web.Views.Shared.Components.SideBarNav
             _abpSession = abpSession;
         }
 
+        /// <summary>
+        /// 调用 异步方法
+        /// </summary>
+        /// <param name="activeMenu">激活的菜单</param>
+        /// <returns></returns>
         public async Task<IViewComponentResult> InvokeAsync(string activeMenu = "")
         {
             var model = new SideBarNavViewModel
